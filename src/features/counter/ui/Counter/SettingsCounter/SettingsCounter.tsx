812bca506e9bf8f618dfea981272b6f06914ee84
botr1k <a.lavnikevich@gmail.com>
2024-12-27 17:30:06 +0300

    fix changeStartValueHandler and changeMaxValueHandler

--- src/features/counter/ui/Counter/SettingsCounter/SettingsCounter.tsx
+++ src/features/counter/ui/Counter/SettingsCounter/SettingsCounter.tsx
@@ -22,6 +22,10 @@ export const SettingsCounter = (props: Props) => {
   const dispatch = useAppDispatch()
 
 
+  const [localStartValue, setLocalStartValue] = useState<string>(startValue.toString());
+  const [localMaxValue, setLocalMaxValue] = useState<string>(maxValue.toString());
+
+
   const setStartValue = (startValue: number) => {
     dispatch(setStartValueAC(startValue))
   }
@@ -31,24 +35,38 @@ export const SettingsCounter = (props: Props) => {
   }
 
   const changeStartValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const newValue = +event.currentTarget.value;
-    if (newValue >= 0 && newValue < maxValue) {
-      setError(null);
-      setStartValue(newValue);
+    const newValue = event.currentTarget.value;
+    setLocalStartValue(newValue)
+
+    if (newValue === '') {
+      setError(null)
+      return
+    }
+
+    if (+newValue >= 0 && +newValue < maxValue) {
+      setError(null)
+      setStartValue(+newValue)
     } else {
       setError('Начальное значение должно быть больше или равно 0\u00A0и\u00A0меньше максимального значения.');
     }
-  };
+  }
 
   const changeMaxValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const newValue = +event.currentTarget.value;
-    if (newValue >= 0 && newValue > startValue) {
+    const newValue = event.currentTarget.value;
+    setLocalMaxValue(newValue)
+
+    if (newValue === '') {
       setError(null);
-      setMaxValue(newValue);
+      return;
+    }
+
+    if (+newValue >= 0 && +newValue > startValue) {
+      setError(null);
+      setMaxValue(+newValue);
     } else {
       setError('Максимальное значение должно быть больше или равно 0\u00A0и\u00A0больше начального значения.');
     }
-  };
+  }
 
   return (
       <div className="space-y-4">
@@ -61,12 +79,12 @@ export const SettingsCounter = (props: Props) => {
 
         <div>
           <label className='block text-sm font-medium text-gray-700' htmlFor='min'>Минимальное значение</label>
-          <Input id='min' type='number' value={startValue} onChange={changeStartValueHandler}/>
+          <Input id='min' type='number' value={localStartValue} onChange={changeStartValueHandler}/>
         </div>
 
         <div>
           <label className='block text-sm font-medium text-gray-700' htmlFor='max'>Максимальное значение</label>
-          <Input id='max' type="number" value={maxValue} onChange={changeMaxValueHandler}/>
+          <Input id='max' type="number" value={localMaxValue} onChange={changeMaxValueHandler}/>
         </div>
 
         <Button className='w-full' onClick={toggleSettings} disabled={!!error}>Сохранить и вернуться</Button>
